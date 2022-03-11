cd3989544cead37483e2ffc64a08fb05673f24bb
Lukas Eber <lukaseber9@gmail.com>
2022-03-11 23:54:40 +0100

    Hangman GUI

--- Minigames/Pages/Hangman.razor.cs
+++ Minigames/Pages/Hangman.razor.cs
@@ -1,20 +1,12 @@
-﻿using System.Diagnostics;
+﻿
 
 namespace Minigames.Pages
 {
     public partial class Hangman
     {
         List<char> letters = new List<char>() { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        List<string> wrongLetters = new List<string>() {"a", "d" };
 
-        public void StartEasyGame()
-        {
-            
-        }
-
-        public void StartHardGame()
-        {
-
-        }
         public void PlayerWon()
         {
 
---
+++ Minigames/Pages/Hangman1.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace Minigames.Pages
+{
+    public partial class Hangman1
+    {
+        public void StartEasyGame()
+        {
+
+        }
+
+        public void StartHardGame()
+        {
+
+        }
+    }
+}
