d2c8426ba5fe52b76215aeb54e5d1e41d8960a82
Lukas Eber <lukaseber9@gmail.com>
2022-04-09 23:07:35 +0200

    Finished Hangman game

--- Minigames/Hangman Classes/RandomWord.cs
+++ Minigames/Hangman Classes/RandomWord.cs
@@ -4,9 +4,9 @@ namespace Minigames
 {
     public class RandomWord
     {
-      public string CurrentWord { get; set; }
+        public string CurrentWord { get; set; }
 
-        private List<string> wordsEasy = new List<string>
+        private List<string> words = new List<string> ()
         {
              "time", "year", "people", "way", "day", "man", "thing", "woman", "life", "child", "world", "school", "state",
              "family", "student", "group", "problem", "hand", "part", "place", "case", "week", "system",
@@ -14,28 +14,19 @@ namespace Minigames
              "money", "story", "fact", "month", "lot", "right", "study", "book", "eye", "job", "word", "issue", "side",
              "kind", "head", "house", "friend", "father", "power", "hour", "game", "line", "end", "member", "law", "car",
              "city", "name", "team", "minute", "idea", "kid", "body", "back", "parent",
-             "face", "others", "level", "office", "door", "person", "art", "war", "history", "party", "result", "reason"
+             "face", "others", "level", "office", "door", "person", "art", "war", "history", "party", "result", "reason",
+             "country",  "company", "government", "community", "health", "president", "question", "business", "service",
+             "information", "absolutely", "academic", "accomplish", "basically", "campaign", "environmental", "capability"
         };
 
-        private List<string> wordsHard = new List<string>
-        {
-            "country",  "company", "government", "community", "health", "president", "question", "business", "service",
-            "information", "absolutely", "academic", "accomplish", "basically", "campaign", "environmental", "capability"
-        };
-        public void GenerateEasyRandomWord()
+        
+        public void GenerateRandomWord()
         {
             Random rnd = new Random();
-            int index = rnd.Next(wordsEasy.Count());
-            string randomWord = wordsEasy[index];
-            this.CurrentWord = randomWord;
+            int index = rnd.Next(words.Count());
+            this.CurrentWord = words[index];
         }
 
-        public void GenerateDifficultRandomWord()
-        {
-            Random rnd = new Random();
-            int index = rnd.Next(wordsHard.Count());
-            string randomWord = wordsHard[index];
-            this.CurrentWord = randomWord;
-        }
+        
     }
 }
--- Minigames/Pages/Hangman.razor.cs
+++ Minigames/Pages/Hangman.razor.cs
@@ -1,12 +1,98 @@
-﻿
-
-namespace Minigames.Pages
+﻿namespace Minigames.Pages
 {
     public partial class Hangman
     {
-        public string Word { get; set; }
+        private string word;
+        private List<string> UnderscoreWord = new List<string>() { };
+        private List<char> letters = new List<char>() { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        private List<char> wrongLetters = new List<char>() { };
+        private List<string> remainingAttempts = new List<string>() { "❌", "❌", "❌", "❌", "❌" };
 
-        List<char> letters = new List<char>() { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-        List<string> wrongLetters = new List<string>() { "a", "d" };
+        protected override void OnInitialized()
+        {
+            ReplaceLetterToUnderscore();
+            SetFirstLetter();
+            base.OnInitialized();
+        }
+        public void GetWord()
+        {
+            RandomWord randomword = new RandomWord();
+            randomword.GenerateRandomWord();
+            word = randomword.CurrentWord;
+
+        }
+        public void ReplaceLetterToUnderscore()
+        {
+            GetWord();
+            int count = 0;
+            while (count < this.word.Length)
+            {
+                UnderscoreWord.Add("▃");
+                count++;
+            }
+        }
+        public void ReadLetter(char letter)
+        {
+            if (word.Contains(letter))
+            {
+                var foundIndexes = new List<int>();
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (word[i] == letter)
+                        foundIndexes.Add(i);
+                }
+                foreach (var i in foundIndexes)
+                {
+                    UnderscoreWord[i] = letter.ToString();
+                }
+                PlayerWon();
+            }
+            else
+            {
+                if (!wrongLetters.Contains(letter))
+                {
+                    remainingAttempts.RemoveAt(1);
+                    AddWrongLetter(letter);
+                    PlayerLost();
+                }
+            }
+        }
+        public void PlayerWon()
+        {
+            if (!UnderscoreWord.Contains("▃"))
+            {
+                remainingAttempts.Clear();
+                remainingAttempts.Add("✯ You won! ✯");
+                letters.Clear();
+            }
+        }
+        public void NewGame()
+        {
+            wrongLetters.Clear();
+            UnderscoreWord.Clear();
+            List<string> remaining = new List<string>() { "❌", "❌", "❌", "❌", "❌" };
+            List<char> let = new List<char>() { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            remainingAttempts = remaining;
+            letters = let;
+            OnInitialized();
+        }
+        public void PlayerLost()
+        {
+            if (wrongLetters.Count == 4)
+            {
+                remainingAttempts.Clear();
+                remainingAttempts.Add($" You lost!  the word was {word}.");
+                letters.Clear();
+            }
+        }
+        public void SetFirstLetter()
+        {
+            int index = word.IndexOf(word.First());
+            UnderscoreWord[0] = word[index].ToString();
+        }
+        public void AddWrongLetter(char letter)
+        {
+            wrongLetters.Add(letter);
+        }
     }
 }
